bc0f1ab868d8ecdab1f365542885ed6845d79cce
Johns Beharry <johns@peakshift.com>
2021-02-16 05:30:18 +0100

    feat(typings/specter): add app configs

--- packages/typings/specter.d.ts
+++ packages/typings/specter.d.ts
@@ -1,5 +1,5 @@
 declare module Specter {
-	// The apps settings, there iss one instance
+	// The app's settings, there iss one instance
     export interface AppSettings {
 		mode: "specterd" | "hwibridge";
         specterURL: string;
@@ -20,6 +20,24 @@ declare module Specter {
 		fullpath: string;
 	}
 
+	// The app's configurations
+    export interface Config {
+        rpc: Rpc;
+        auth: string;
+        explorers: Explorers;
+        proxy_url: string;
+        only_tor: boolean;
+        tor_control_port: string;
+        hwi_bridge_url: string;
+        uid: string;
+        unit: string;
+        price_check: boolean;
+        alt_rate: number;
+        alt_symbol: string;
+        price_provider: string;
+        validate_merkle_proofs: boolean;
+    }
+
 	export interface Key {
 		original: string;
 		fingerprint: string;
@@ -28,4 +46,21 @@ declare module Specter {
 		purpose: string;
 		xpub: string;
 	}
+
+    export interface Rpc {
+        autodetect: boolean;
+        datadir: string;
+        user: string;
+        password: string;
+        port: string;
+        host: string;
+        protocol: string;
+    }
+
+    export interface Explorers {
+        main: string;
+        test: string;
+        regtest: string;
+        signet: string;
+    }
 }
